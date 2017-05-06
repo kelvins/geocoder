73976853967234d57553d7a960304a1c75f8a220
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-05-06 12:26:45 -0300

    Indenting code with gofmt

--- structs/structs.go
+++ structs/structs.go
@@ -4,42 +4,42 @@ package structs
 
 // All results from the JSON
 type Results struct {
-   Results []Result `json:"results"`
-   Status  string   `json:"status"`
+	Results []Result `json:"results"`
+	Status  string   `json:"status"`
 }
 
 // Each result from the JSON
 type Result struct {
-   AddressComponents []Address `json:"address_components"`
-   FormattedAddress  string    `json:"formatted_address"`
-   Geometry          Geometry  `json:"geometry"`
-   PlaceId           string    `json:"place_id"`
-   Types             []string  `json:"types"`
+	AddressComponents []Address `json:"address_components"`
+	FormattedAddress  string    `json:"formatted_address"`
+	Geometry          Geometry  `json:"geometry"`
+	PlaceId           string    `json:"place_id"`
+	Types             []string  `json:"types"`
 }
 
 // Each address is identified by the 'types'
 type Address struct {
-   LongName  string   `json:"long_name"`
-   ShortName string   `json:"short_name"`
-   Types     []string `json:"types"`
+	LongName  string   `json:"long_name"`
+	ShortName string   `json:"short_name"`
+	Types     []string `json:"types"`
 }
 
 // Each value in the geometry
 type Geometry struct {
-   Bounds       Bounds `json:"bounds"`
-   Location     LatLng `json:"location"`
-   LocationType string `json:"location_type"`
-   Viewport     Bounds `json:"viewport"`
+	Bounds       Bounds `json:"bounds"`
+	Location     LatLng `json:"location"`
+	LocationType string `json:"location_type"`
+	Viewport     Bounds `json:"viewport"`
 }
 
 // Bounds Northeast and Southwest
 type Bounds struct {
-   Northeast LatLng `json:"northeast"`
-   Southwest LatLng `json:"southwest"`
+	Northeast LatLng `json:"northeast"`
+	Southwest LatLng `json:"southwest"`
 }
 
 // Latitude and Longitude
 type LatLng struct {
-   Lat float64 `json:"lat"`
-   Lng float64 `json:"lng"`
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
