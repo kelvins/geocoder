0b07e7537247145b29c796ae41afd0bb51d43490
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-05-06 12:25:14 -0300

    Added structs for the google geocoding API

---
+++ structs/structs.go
@@ -0,0 +1,45 @@
+// Package structs is used to define all structures that
+// are used to get the results in the JSON
+package structs
+
+// All results from the JSON
+type Results struct {
+   Results []Result `json:"results"`
+   Status  string   `json:"status"`
+}
+
+// Each result from the JSON
+type Result struct {
+   AddressComponents []Address `json:"address_components"`
+   FormattedAddress  string    `json:"formatted_address"`
+   Geometry          Geometry  `json:"geometry"`
+   PlaceId           string    `json:"place_id"`
+   Types             []string  `json:"types"`
+}
+
+// Each address is identified by the 'types'
+type Address struct {
+   LongName  string   `json:"long_name"`
+   ShortName string   `json:"short_name"`
+   Types     []string `json:"types"`
+}
+
+// Each value in the geometry
+type Geometry struct {
+   Bounds       Bounds `json:"bounds"`
+   Location     LatLng `json:"location"`
+   LocationType string `json:"location_type"`
+   Viewport     Bounds `json:"viewport"`
+}
+
+// Bounds Northeast and Southwest
+type Bounds struct {
+   Northeast LatLng `json:"northeast"`
+   Southwest LatLng `json:"southwest"`
+}
+
+// Latitude and Longitude
+type LatLng struct {
+   Lat float64 `json:"lat"`
+   Lng float64 `json:"lng"`
+}
