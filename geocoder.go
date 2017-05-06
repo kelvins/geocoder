8e4ca7c30b4a02c1713eaac403489811f3ae5906
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-05-06 17:25:08 -0300

    Minor code improvements and added some comments :art:

--- geocoder.go
+++ geocoder.go
@@ -1,19 +1,20 @@
-// Package geocoder
+// Package geocoder provides an easy way to use the Google Geocoding API
 package geocoder
 
 import (
-	"log"
-	"errors"
-	"strings"
-	"strconv"
-	"net/http"
 	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/kelvins/geocoder/structs"
 )
 
-var geocodeApiUrl string
+// The user should set the API KEY provided by Google
 var ApiKey string
+var geocodeApiUrl string
 
 // Define the Geocode API URL
 func init() {
@@ -22,21 +23,21 @@ func init() {
 
 // Address structure used in the Geocoding and GeocodingReverse functions
 type Address struct {
-	Street      string
-	Number      int
-	District    string
-	City        string
-	County      string
-	State       string
-	Country     string
-	PostalCode  string
-	Types       string
+	Street     string
+	Number     int
+	District   string
+	City       string
+	County     string
+	State      string
+	Country    string
+	PostalCode string
+	Types      string
 }
 
 // Location structure used in the Geocoding and GeocodingReverse functions
 type Location struct {
-   Latitude  float64
-   Longitude float64
+	Latitude  float64
+	Longitude float64
 }
 
 // Format an address based on the Address structure
@@ -57,14 +58,15 @@ func FormatAddress(address Address) string {
 	content = append(content, address.Country)
 
 	var formatedAddress string
+
 	// For each value in the content slice check if it is valid
 	// and add to the formatedAddress string
-	for index := 0; index < len(content); index++ {
-		if content[index] != "" {
+	for _, value := range content {
+		if value != "" {
 			if formatedAddress != "" {
 				formatedAddress += ", "
 			}
-			formatedAddress += content[index]
+			formatedAddress += value
 		}
 	}
 
@@ -99,7 +101,7 @@ func httpRequest(url string) (structs.Results, error) {
 	defer resp.Body.Close()
 
 	// Use json.Decode for reading streams of JSON data
-	err = json.NewDecoder(resp.Body).Decode(&results);
+	err = json.NewDecoder(resp.Body).Decode(&results)
 	if err != nil {
 		return results, err
 	}
@@ -131,10 +133,13 @@ func Geocoding(address Address) (Location, error) {
 
 	// Create the URL based on the formated address
 	url := geocodeApiUrl + "address=" + formatedAddress
+
+	// Use the API Key if it was set
 	if ApiKey != "" {
 		url += "&key=" + ApiKey
 	}
 
+	// Send the HTTP request and get the results
 	results, err := httpRequest(url)
 	if err != nil {
 		log.Println(err)
@@ -142,8 +147,8 @@ func Geocoding(address Address) (Location, error) {
 	}
 
 	// Get the results (latitude and longitude)
-	location.Latitude  = results.Results[0].Geometry.Location.Lat;
-	location.Longitude = results.Results[0].Geometry.Location.Lng;
+	location.Latitude = results.Results[0].Geometry.Location.Lat
+	location.Longitude = results.Results[0].Geometry.Location.Lng
 
 	return location, nil
 }
@@ -154,32 +159,33 @@ func convertResultsToAddress(results structs.Results) (addresses []Address) {
 	for index := 0; index < len(results.Results); index++ {
 		var address Address
 
-		for component := 0; component < len(results.Results[index].AddressComponents); component++ {
+		// Put each component from the AddressComponents slice in the correct field in the Address structure
+		for _, component := range results.Results[index].AddressComponents {
 
-			switch results.Results[index].AddressComponents[component].Types[0] {
+			switch component.Types[0] {
 			case "route":
-				address.Street = results.Results[index].AddressComponents[component].LongName
+				address.Street = component.LongName
 				break
 			case "street_number":
-				address.Number, _ = strconv.Atoi(results.Results[index].AddressComponents[component].LongName)
+				address.Number, _ = strconv.Atoi(component.LongName)
 				break
 			case "locality":
-				address.District = results.Results[index].AddressComponents[component].LongName
+				address.District = component.LongName
 				break
 			case "administrative_area_level_3":
-				address.City = results.Results[index].AddressComponents[component].LongName
+				address.City = component.LongName
 				break
 			case "administrative_area_level_2":
-				address.County = results.Results[index].AddressComponents[component].LongName
+				address.County = component.LongName
 				break
 			case "administrative_area_level_1":
-				address.State = results.Results[index].AddressComponents[component].LongName
+				address.State = component.LongName
 				break
 			case "country":
-				address.Country = results.Results[index].AddressComponents[component].LongName
+				address.Country = component.LongName
 				break
 			case "postal_code":
-				address.PostalCode = results.Results[index].AddressComponents[component].LongName
+				address.PostalCode = component.LongName
 				break
 			default:
 				break
@@ -199,21 +205,26 @@ func GeocodingReverse(location Location) ([]Address, error) {
 
 	var addresses []Address
 
-	// Create the URL based on the formated address
-	latitude  := strconv.FormatFloat(location.Latitude, 'f', 8, 64)
+	// Convert the latitude and longitude from double to string
+	latitude := strconv.FormatFloat(location.Latitude, 'f', 8, 64)
 	longitude := strconv.FormatFloat(location.Longitude, 'f', 8, 64)
 
+	// Create the URL based on latitude and longitude
 	url := geocodeApiUrl + "latlng=" + latitude + "," + longitude
+
+	// Use the API key if it was set
 	if ApiKey != "" {
 		url += "&key=" + ApiKey
 	}
 
+	// Send the HTTP request and get the results
 	results, err := httpRequest(url)
 	if err != nil {
 		log.Println(err)
 		return addresses, err
 	}
 
+	// Convert the results to an Address slice called addresses
 	addresses = convertResultsToAddress(results)
 
 	return addresses, nil
